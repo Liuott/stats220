3d8149e39e9bd2d9bc1525d4bc0ddadab3495829
Liuott <101920214+Liuott@users.noreply.github.com>
2022-03-20 16:29:53 +0800

    Update meme.R

--- meme.R
+++ meme.R
@@ -9,7 +9,7 @@ backdrop<-image_blank(width = 600,height = 600,color = "#003300") %>%
 backdrop
 Teletubbies<-image_read("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.2qqtouxiang.com%2Fpic%2FTX9978_02.jpg&refer=http%3A%2F%2Fimg.2qqtouxiang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1650354145&t=929c18b589cd8d59a4be6cdc4dc1424a")
 Teletubbies<-image_annotate(Teletubbies,"Peaceful!!!",color = "White", size=60,location = "+220+500")
-Teletubbies<-image_scale(Teletubbies,"x500")
+Teletubbies<-image_scale(Teletubbies,"x600")
 print(Teletubbies)
 
 calm<-image_read("https://cdn.tatlerasia.com/asiatatler/i/hk/2020/04/05042901-sage-friedman-hs5clnqbcoc-unsplash_cover_1920x1280.jpg")
@@ -20,4 +20,5 @@ print(calm)
 meme<-c(backdrop,Teletubbies,calm)%>%
   image_append((stack=FALSE))%>%
   image_scale(1000)
-print(meme)
+meme
+image_write(meme, "my_meme.png")
